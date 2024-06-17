ee21f31c28208e0539004560b083260ee1a097e1
asheesh kushwah <asheesh94065@gmail.com>
2024-06-17 17:58:58 +0530

    mon

---
+++ set.js
@@ -0,0 +1,13 @@
+document.getElementById("a").innerHTML =
+    "<center><h2>pn infosys gwalior 1234 </h2></center>";
+
+document.getElementById("a").setAttribute("style", "border 10px dotted red")
+
+document.getElementById("a").removeAttribute("style");
+document.getElementById("b").innerHTML = "<h1>gwalior</h1>"
+
+
+//query selector
+document.querySelector("#a").innerHTML = "<center><h2>pn infosys gwalior 1234 </h2></center>"
+document.querySelector(".c").innerHTML = "<h1>gwalior</h1>"
+
