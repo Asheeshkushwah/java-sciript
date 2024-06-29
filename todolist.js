a64a62e7a2a10bf2738dc8e52a8a31111a0c800b
asheesh kushwah <asheesh94065@gmail.com>
2024-06-29 15:49:46 +0530

    satur

--- todolist.js
+++ todolist.js
@@ -1,86 +1,72 @@
-// const userNameTextField = document.getElementById('username')
-// const addUserBtn = document.getElementById('addUser')
-// const recordsDisplay = document.getElementById('records')
-// //get data
-// let userArray = []
+const usernameTextField = document.getElementById("username");
+const addUserBtn = document.getElementById("addUser");
+const recordsDisplay = document.getElementById("records");
 
-// addUserBtn.onclick = () => {
-//     const name = userNameTextField.value
-//     // alert(name)
-//     userArray.push({ 'name': name })
-//     // console.log(userArray)
-//     savedata(userArray)
-// }
-// function savedata(userArray) {
-//     // console.log(userArray)
-//     let str = JSON.stringify(userArray)  //string
-//     // console.log(str)
-//     localStorage.setItem('users', str)  //string format
-//     // displaydata()
-// }
-
-// function displaydata() {
-//     let data = ''
-//     userArray.forEach((item, i) => {
-//         data += `  <tr>
-// <th>${i + 1}</th>
-// <td>${item.name}</td>
-// <td>
-// <i class ="btn text-white fa fa-edit btn-info mx-2"
-// onclick ='EditInfo(${i})'></i>
-// <i class ="btn   btn-danger   text-white fa fa-trash"
-// onclick ='DeletInfo(${i})'></i>
-
-// `
-//         console.log(data)
-//     })
-//     recordsDisplay.innerHtml = data;
-// }
-
-
-
-
-const userNameTextField = document.getElementById('username')
-const addUserBtn = document.getElementById('addUser')
-const recordDisplay = document.getElementById('records')
-
-let userArray = []
-
-//get data local storage
-
-let data = localStorage.getItem('users')
-if (data) {
-    userArray = JSON.parse(data)
-    displayData()
+//get data
+let userArray = [];
+let edit_id = null;
+// gte data local storage
+let objstr = localStorage.getItem("users");
+//console.log(objstr)
+if (objstr != null) {
+    userArray = JSON.parse(objstr); // string to object
+    //console.log(UserArray)
 }
+displayData();
 addUserBtn.onclick = () => {
-    const name = userNameTextField.value
-    userArray.push({ 'name': name })
-    saveData(userArray)
-}
+    const name = usernameTextField.value;
+    // const name = userNameTextField.value.trim();
+    if (name === "") return; // Prevent adding empty names
+
+    if (edit_id != null) {
+        userArray.splice(edit_id, 1, {
+            name: name,
+
+        });
+        edit_id = null;
+        addUserBtn.innerText = 'Add Text';
+    } else userArray.push({ name: name });
+    // alert(name)
+    // userArray.push({ 'name': name });
+    // console.log(userArray)
+    saveData(userArray);
+    usernameTextField.value = ""; //data empty
+};
 
 function saveData(userArray) {
-    //console.log(userArray)
-    let str = JSON.stringify(userArray)//string
+    // console.log(userArray)
+    let str = JSON.stringify(userArray); //string
     //console.log(str)
-    localStorage.setItem('users', JSON.stringify(userArray)) //string format
-    displayData()
 
+    localStorage.setItem("users", str); // string formate
+    displayData();
 }
 
 function displayData() {
-    let data = ''
+    let data = "";
     userArray.forEach((item, i) => {
-        // console.log(i)
         data += `<tr>
-    <th>${i + 1}</th>
-    <td>${item.name}</td>
-    <td>
-    <i class="btn text-white fa fa-edit btn-info mx-2"onclick=" EditInfo(${i})"></i>
-    <i class="btn btn-danger text-white fa fa-trash"onclick=" DeleteInfo(${1})"></i>
-    </td>
-    </tr>`;
-        console.log(data)
-    })
-    recordDisplay.innerHTML = data;
+<th>${i + 1}</th>
+<td>${item.name}</td>
+<td>
+<i class = "btn text-white fa fa-edit btn-warning mx-2"
+onclick='EditInfo(${i})'></i>
+<i class = "btn btn-success text-white fa fa-trash"
+onclick='DeleteInfo(${i})'><i/>
+</td>
+</tr>`;
+        //  console.log(data)
+    });
+    recordsDisplay.innerHTML = data;
+}
+function DeleteInfo(id) {
+    userArray.splice(id, 1); // Remove 1 element at index id
+    saveData(userArray);
+}
+
+function EditInfo(id) {
+    // alert(id)
+    edit_id = id;
+    usernameTextField.value = userArray[id].name;
+    addUserBtn.innerText = "Updateuser";
 }
--- todolistpractice.js
+++ todolistpractice.js
@@ -26,43 +26,43 @@ const userNameTextField = document.getElementById('username')
 const addUserBtn = document.getElementById('addUser')
 const recordDisplay = document.getElementById('records')
 
-let userArray=[]
+let userArray = []
 
 //get data local storage
 
 let data = localStorage.getItem('users')
-if(data){
+if (data) {
     userArray = JSON.parse(data)
     displayData()
 }
 addUserBtn.onclick = () => {
     const name = userNameTextField.value
-    userArray.push({'name':name})
+    userArray.push({ 'name': name })
     saveData(userArray)
 }
 
-function saveData(userArray){
+function saveData(userArray) {
     //console.log(userArray)
     let str = JSON.stringify(userArray)//string
     //console.log(str)
-    localStorage.setItem('users',JSON.stringify(userArray)) //string format
+    localStorage.setItem('users', JSON.stringify(userArray)) //string format
     displayData()
 
 }
 
-function displayData(){
-  let data  = ''
-  userArray.forEach((item,i)=>{
-    // console.log(i)
-    data += `<tr>
-    <th>${i+1}</th>
+function displayData() {
+    let data = ''
+    userArray.forEach((item, i) => {
+        // console.log(i)
+        data += `<tr>
+    <th>${i + 1}</th>
     <td>${item.name}</td>
     <td>
     <i class="btn text-white fa fa-edit btn-info mx-2"onclick=" EditInfo(${i})"></i>
     <i class="btn btn-danger text-white fa fa-trash"onclick=" DeleteInfo(${1})"></i>
     </td>
     </tr>`;
-    console.log(data)
-})
-recordDisplay.innerHTML = data;
+        console.log(data)
+    })
+    recordDisplay.innerHTML = data;
 }
