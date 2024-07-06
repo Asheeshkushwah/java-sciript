8f84fe20a859a81d8d212c63af7a7023fae32983
asheesh kushwah <asheesh94065@gmail.com>
2024-07-06 15:25:23 +0530

    wshuhk

--- weather.js
+++ weather.js
@@ -1,28 +1,26 @@
-const apiKey = "aab807cf44cdec0cbd4fb016a295ef18"
 
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q="
-
-
-
+const apiKey = "08aad07b15dd8b555f4185304c7001a5";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 
+// weather image update
+// const weatherIcon = document.querySelector(".weather-icon");
 
-async function checkweather(city) {
+async function checkWeather(city) {
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-    // const response = await fetch(apiUrl + `&appid=${apiKey}`);
+
+    //const response = await fetch(apiUrl + &appid=${apiKey});
     var data = await response.json();
-    // console.log(data);
-
+    //console.log(data);
+    //weather update
     document.querySelector(".city").innerHTML = data.name;
-    document.querySelector("temp").innerHTML = math.round(data.main.temp) + "";
-    document.querySelector("humidity").innerHTML = data.main.humidity + "%";
-    document.querySelector("wind").innerHTML = data.wind.speed + "km/h";
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
 }
-
-// checkweather()
-
+//checkWeather()
 searchBtn.addEventListener("click", () => {
-    checkweather(searchBox.value);
+    checkWeather(searchBox.value);
 })
